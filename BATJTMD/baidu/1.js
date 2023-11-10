20125c601978d3e98471f328b6cb272e04579eeb
skyfker <2095587752@qq.com>
2023-11-10 23:48:02 +0800

    设计模型

---
+++ BATJTMD/baidu/1.js
@@ -0,0 +1,33 @@
+// ws6 类
+// 类和对象有什么区别
+// 类是抽象的，定制
+// 对象是具体的
+class SingelDog{
+    constructor(){
+
+    }
+    //static instance = null
+    // 属于类上的方法
+    static getInstance(){
+        // console.log('类的方法');
+        if(!SingelDog.instance){
+            //仅实例化一次
+            SingelDog.instance = new SingelDog()
+        }
+        return SingelDog.instance
+    }
+    // 公有方法 属于实例上的
+    show(){
+        console.log('单身贵族');
+    }
+}
+// 设计模式 一个类只实例化一次 封装
+// 第一次 new  ，之后不实例化，直接返回实例
+// new 角度 搞不定？ &{}
+// 总领导 弹窗 
+const s1 = SingelDog.getInstance();  //生成对象->拿到对象
+const s2 = SingelDog.getInstance();  
+// // 如何让他们相等
+// console.log(s1 === s2); //false  内存中不一样
+
+// console.log(SingelDog.getInstance());
