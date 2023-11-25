b5242a4446f621cb1ff7302e6691b76920b36ba5
skyfker <2095587752@qq.com>
2023-11-25 21:19:31 +0800

    回文

---
+++ leetcode/字符串/1.js
@@ -0,0 +1,9 @@
+let str = 'hello'
+
+// console.log(str.art(1));
+// console.log(str[1]);
+
+const arr = str.split('') //['h', 'e', 'l', 'l', 'o']
+const arr2 = str.split('e') //['h', 'llo']
+const s = str.split('').reverse().join('')
+
