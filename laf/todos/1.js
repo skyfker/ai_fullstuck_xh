e3dd2235a5035948b8a38c839cc2f30d474c0405
skyfker <2095587752@qq.com>
2023-11-30 22:50:40 +0800

    laf 云开发初体验

---
+++ laf/todos/1.js
@@ -0,0 +1,18 @@
+//立即执行函数
+//async 函数修饰符 不让这个函数内部有异步
+(async function(){
+    //js是单线程语言  把异步放入event loop 
+    //thenable  异步任务中控制执行顺序
+//   fetch('https://yjhx77.laf.run/get-list').then(res=>
+//   res.json()).then(data=>{
+//   console.log(data);
+//   })
+//异步变同步，有利于流程控制
+//await 后面跟一个promise实例，同步等待被resolve
+//代码的缩写顺序 = 执行顺序
+await 后面跟一个promise实例
+const res=await fetch('https://yjhx77.laf.run/get-list')
+const {data}=await res.json()
+console.log(data);
+console.log('ok');
+})()
