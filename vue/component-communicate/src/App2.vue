37698cadbd8ea00f14146993efb14fcad8a0ff52
skyfker <2095587752@qq.com>
2023-12-21 22:47:29 +0800

    组件通信

---
+++ vue/component-communicate/src/App2.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="head">
+        <input type="text" v-model="message">
+        <button @click="submit">确定</button>
+    </div>
+    <list :lists="lists"/>
+
+</template>
+
+<script>
+import list from './components/body2/list.vue'
+export default {
+    components:{
+        list
+    },
+    data() {
+        return {
+            lists: ['html', 'css'],
+            message: ''
+        }
+    },
+    methods: {
+        submit() {
+            if(this.message) {
+                this.lists.push(this.message)
+                this.message = ''
+            }
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped></style>
