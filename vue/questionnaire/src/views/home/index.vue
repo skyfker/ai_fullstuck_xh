f57955652a22da3d1e0c5b76a10c4e3304079a09
skyfker <2095587752@qq.com>
2023-12-22 21:23:40 +0800

    问卷调查开发，day1

---
+++ vue/questionnaire/src/views/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <ItemContainer />
+    </div>
+</template>
+
+<script>
+import ItemContainer from '@/components/itemContainer.vue'
+export default {
+    components: {
+        ItemContainer
+    }
+}
+</script>
+
+<style lang="less"></style>
