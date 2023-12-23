b86fcf535f9f58a07f3f3d16485dc17e9492debd
skyfker <2095587752@qq.com>
2023-12-23 23:58:46 +0800

    问卷调查开发，day2

--- vue/questionnaire/src/views/item/index.vue
+++ vue/questionnaire/src/views/item/index.vue
@@ -1,15 +1,25 @@
 <template>
     <div>
-        item
+        <ItemContainer parentComponent="item"/>
     </div>
 </template>
 
 <script>
+    import ItemContainer from '@/components/itemContainer.vue';
+    import { mapActions } from 'vuex';
     export default {
-        
+        components: {
+            ItemContainer
+        },
+        created() { //生命周期函数 在页面加载的过程中自动会触发
+            this.getQuestionListAction()
+        },
+        methods: {
+            ...mapActions(['getQuestionListAction'])
+        }
     }
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
 
 </style>
